d7ce8340462336dfad84c49475d222d35b793bd9
Hunter McCarthy <huntermccarthy56@hotmail.com>
2023-02-02 19:24:10 -0500

    added a bunch of JS functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,75 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// // the code isn't run until the browser has finished rendering all the elements
+// // in the html.
+
+
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+
+var now = dayjs()
+console.log(now);
+
+$('div').each(function(index) {
+  
+if ($(this).hasClass('row')) {
+
+  // console.log($(this))
+
+  if($(this).children(":first").data('hour') > dayjs().hour()) {
+
+    $(this).removeClass(' past present ');
+
+    $(this).addClass('future')
+
+  } else if ( $(this).children(":first").data('hour') < dayjs().hour() ) {
+
+    $(this).removeClass(' future present ');
+
+    $(this).addClass('past')
+
+  } else {
+
+    $(this).removeClass(' future past ');
+    $(this).addClass('present')
+}}}) 
+
+for (var i = 9; i <= 17; i++) {
+
+  if (localStorage.getItem(i) == null) {
+
+    localStorage.setItem(i, "")
+  } else {
+  //  $('#' + i).css('background-color', 'red')
+
+  // console.log($('#' + i))
+  // // console.log(localStorage.getItem(i))
+
+document.getElementById(i).value = localStorage.getItem(i)
+
+  }
+  
+
+}
+
+})
+
+
+
+$('.time-block').click( function() {
+
+  // console.log(event.target.classList.contains('saveBtn'))
+
+  if (event.target.classList.contains('saveBtn')) {
+
+    localStorage.setItem($(this).children('div').first().data('hour'), 
+    document.getElementById(($(this).children('div').first().data('hour'))).value )
+    
+    console.log($(this).children('div').first().data('hour'))
+
+    document.getElementById(($(this).children('div').first().data('hour'))).value 
+
+
+  
+
+}  })
+  
